55b6d263d76e166b4b9c2eb5da9378992d9834b8
a7xsalvador <30467638+a7xsalvador@users.noreply.github.com>
2021-09-16 19:28:54 -0500

    Two new functions

--- a.py
+++ a.py
@@ -14,6 +14,27 @@ def tokenize(corpus):
 
     return tokenized
 
+def word_frequency(token):
+    dic={}
+    for word in token:
+        if word not in dic:
+            dic[word]=1
+        else:
+            dic[word] += 1
+    return dic
+
+def sort_dic(dic):
+    dic_sorted = {}
+    dic_values = list(sorted(dic.values()))
+    index = 0
+    while len(dic) != len(dic_sorted):
+        for i in dic.keys():
+            if dic[i] == dic_values[index]:
+                dic_sorted[i]=dic[i]
+                index += 1
+    return dic_sorted
+
+
 
 def start():
     corpus = str(input("please enter a text "))
